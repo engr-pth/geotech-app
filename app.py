fcac8540ce469f56b7adf470fc4bf8fa2a65b798
engr-pth <engrpth@gmail.com>
2026-08-06 11:50:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ st.markdown("""
 ဤ Web App သည် Geotechnical Engineering တွက်ချက်မှုများအတွက် Tool စုံလင်စွာ ပါဝင်သော Platform ဖြစ်ပါသည်။ 
 
 👈 **ဘေးဘက် Sidebar မီနူးမှ မိမိအသုံးပြုလိုသည့် Tool ကို ရွေးချယ်ပါ:**
-* **Soil Classification:** Grain size distribution နှင့် Atterberg limits များဖြင့် မြေဆီလွှာအမျိုးအစားခွဲရန်
-* **Single Footing Design:** Shallow Foundation တစ်ခုဖြစ်သော Single Footing Design တွက်ချက်ရန်
+* **Soil Classification:** Grain size distribution နှင့် Atterberg limits များဖြင့် မြေအမျိုးအစားခွဲရန်
+* **Isolated Footing Design:** Shallow Foundation တစ်ခုဖြစ်သော Isolated Footing Design တွက်ချက်ရန်
+* **Continuous Wall Footing Design:** Shallow Foundation တစ်ခုဖြစ်သော Continuous Wall Footing Design တွက်ချက်ရန်
 """)
